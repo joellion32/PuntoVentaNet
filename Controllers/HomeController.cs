92214792b83e804e4feca29ba13b5ac78d5a0a6a
Miguel Angel Castro <miguelangelpolanco@gmail.com>
2021-05-01 22:26:39 -0400

    En desarrollo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Punto_de_venta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Punto_de_venta.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // mostrar pedidos
+            List<PedidosModel> pedidos;
+            PuntoVentaEntities db = new PuntoVentaEntities();
+            pedidos = (from p in db.Pedidos
+                          join c in db.Clientes
+                          on p.IdPedido equals c.IdCliente
+                          select new PedidosModel
+                          {
+                              IdPedido = p.IdPedido,
+                              Nombre = c.Nombre,
+                              Total = (int)p.Total,
+                          }).ToList();
+
+
+            return View(pedidos);
+        }
+    }
+}
---
+++ Models/PedidosModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Punto_de_venta.Models
+{
+    public class PedidosModel
+    {
+        public int IdPedido { get; set; }
+        public string Nombre { get; set; }
+        public int Total { get; set; }
+    }
+}
